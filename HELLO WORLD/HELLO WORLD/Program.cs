17290b96470dc466f365970ba063a3eb9b28e4f2
jan <91151156+Jnmichael@users.noreply.github.com>
2022-04-11 13:48:51 -0700

    M

---
+++ HELLO WORLD/HELLO WORLD/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine();
+
+Console.WriteLine("    /|");
+Console.WriteLine("   / |");
+Console.WriteLine("  /  |");
+Console.WriteLine(" /___|");
+
+Console.WriteLine();
+
+string name = "Jan";
+int age = 25;
+bool isMale = true;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isMale);
+
+Console.WriteLine();
+
+double MyFloat = 10.1;
+
+Console.WriteLine(MyFloat);
